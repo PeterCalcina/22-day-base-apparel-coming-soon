80f208304ba27b6ad196ebf5d474a8b2b691d607
Peter Calcina <peter.calcina1221@gmail.com>
2024-02-24 19:51:31 -0400

    Corregir validación de campo de correo electrónico en formulario

--- js/script.js
+++ js/script.js
@@ -1,21 +1,15 @@
 let modal = document.querySelector('.modal');
 let email = document.getElementById('email');
+let errorMessage = document.querySelector('.error');
+let iconError = document.querySelector('.icon_error');
 
-document.getElementById("form_coming").addEventListener("submit", function(event) {  
-  let errorMessage = document.querySelector('.error');
-  let iconError = document.querySelector('.icon_error');  
-
+document.getElementById("form_coming").addEventListener("submit", function (event) {
   if (!checkEmail(email.value)) {
-      errorMessage.classList.add("active");
-      iconError.classList.add("active");
-      email.focus();
-      event.preventDefault();
+    showErroMessages(email.value);
   } else {
-      errorMessage.classList.remove("active");
-      iconError.classList.remove("active");
-      modal.classList.add("active");
-      event.preventDefault();
+    clearErrorMessages();
   }
+  event.preventDefault();
 });
 
 function checkEmail(email) {
@@ -23,10 +17,30 @@ function checkEmail(email) {
   return regex.test(email);
 }
 
+function validateEmptyField(value) {
+  return value === "" ?
+    "Email cannot be empty" :
+    "Please provide a valid email";
+}
+
+function showErroMessages(value) {
+  errorMessage.textContent = validateEmptyField(value);
+  
+  errorMessage.classList.add("active");
+  iconError.classList.add("active");
+  email.focus();
+}
+
+function clearErrorMessages() {
+  errorMessage.textContent = "";
+  errorMessage.classList.remove("active");
+  iconError.classList.remove("active");
+  modal.classList.add("active");
+}
+
 let closeModal = document.querySelector('.btn_close');
 
-closeModal.addEventListener('click', function() {
+closeModal.addEventListener('click', function () {
   modal.classList.remove("active");
   email.value = "";
 });
-
